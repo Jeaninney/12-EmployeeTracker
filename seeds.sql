07e2ac701bec7b1bcdd9fd188c9e5c4d0c7fa403
Jeaninne <jeaninnel@hotmail.com>
2020-06-14 22:02:36 -0400

    added sql seeds

---
+++ seeds.sql
@@ -0,0 +1,24 @@
+USE employee_trackerdb;
+
+INSERT into department (dept_name)
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+
+INSERT into role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1),
+    ("Salesperson", 80000, 1),
+    ("Lead Engineer", 150000, 2),
+    ("Software Engineer", 120000, 2),
+    ("Accountant", 125000, 3),
+    ("Legal Team Lead", 250000, 4),
+    ("Lawyer", 190000, 4);
+
+INSERT into employee (first_name, last_name, role_id, department_id)
+VALUES ("John", "Doe", 1, 1),
+    ("Mike", "Chan", 2, 1),
+    ("Ashley", "Rodriguez", 3, 2),
+    ("Kevin", "Tupik", 4, 2),
+    ("Malia", "Brown", 5, 3),
+    ("Sarah", "Lourd", 6, 4),
+    ("Tom", "Allen", 7, 4);
+
